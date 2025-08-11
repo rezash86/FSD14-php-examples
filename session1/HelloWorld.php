6edf7e3771be67f7a152f82f529ca6e71c237cbb
rezash86 <reza.sh@gmail.com>
2025-08-11 19:24:17 -0400

    arrays

--- session1/HelloWorld.php
+++ session1/HelloWorld.php
@@ -1,5 +1,4 @@
 <?php
-
 $group_name = "FSD-14";
 $year = 2025;
 $price = 99.99;
@@ -13,9 +12,17 @@ echo "Name : $name \n";
 echo "Age : $age \n";
 echo "Height : $height \n";
 $status = ($age >= 18) ? "Adult" : "Minor";
-$valueOfStudent=($isStudent ? "yes" : "no");
+$checkStudent=($isStudent ? "yes" : "no");
+print_r($checkStudent);
+echo "\n";
 echo "Is Student :" . ($isStudent ? "yes" : "no") . "\n"; // I use an itenary conditon
 
+$checkStudent=($isStudent ? "is" : "isn't");
+
+print_r($name." is ".$age." yrs old and has ".$height. " ft and ". $checkStudent. " a student");
+
+// please make a sentence with all of the information you put as the values.
+// e.x -> Alice is 3o yrs old and has 1.75 height and is a student
 
 // CTRL + / to make comments
 
---
+++ session1/arraysPractice.php
@@ -0,0 +1,30 @@
+<?php
+$colors = ["red", "green", "blue"];
+echo "the second color -> ". $colors[1] . "\n";
+
+// Get the number of elements
+echo "the count of array -> " . count($colors) . "\n";
+
+//add elements to array
+array_push($colors, "white");
+print_r($colors);
+
+//remove the elements from array
+array_pop($colors);
+print_r($colors);
+
+//remove the element from begining
+$removed_color = array_shift($colors);
+print_r($colors);
+
+//combine arrays
+//Check if a value exists in array
+
+$person=["name"=> "John", "age"=> 30];
+//get all the keys in this array
+
+//get all the values from this data structure
+
+//sort the array
+
+?>
