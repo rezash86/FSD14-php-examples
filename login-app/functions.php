9569746287f00f4e89ee29e1fc49c078d2f5f3c3
rezash86 <reza.sh@gmail.com>
2025-08-19 19:01:56 -0400

    login app - with registration

--- login-app/functions.php
+++ login-app/functions.php
@@ -5,5 +5,10 @@ function is_user_logged_in(){
     return isset($_SESSION["logged_in"]) && $_SESSION["logged_in"] === true;
 }
 
+function user_exists($conn, $username){
+    $sql = "SELECT * FROM users where username='$username' LIMIT 1";
+    $result = mysqli_query($conn, $sql);
+    return mysqli_num_rows($result) > 0;
+}
 
 ?>
